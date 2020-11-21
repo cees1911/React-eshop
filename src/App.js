520e1a0a0a72f4b1e53c991ba79e2368cb15441d
Cees Klap <ceesklap@gmail.com>
2020-11-21 10:26:38 +0100

    data is moved to firebase and 2 bugs are solved

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
@@ -31,12 +32,10 @@ class App extends React.Component {
             ...snapShot.data(),
           });
         });
-      } else {
-        setCurrentUser(userAuth);
       }
+      setCurrentUser(userAuth);
     });
   }
-
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
