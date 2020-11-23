c279870a183003a3141fccf23611751124917c5f
Cees Klap <ceesklap@gmail.com>
2020-11-23 13:46:55 +0100

    included sagas for users and cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSageMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
+import rootSage from './root-saga';
 
-const middlewares = [thunk];
+const sageMiddleware = createSageMiddleware();
+
+const middlewares = [sageMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sageMiddleware.run(rootSage);
 
 export const persistor = persistStore(store);
