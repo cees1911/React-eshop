913a9aa64e8b35cefa7457851a38af392053ef01
Cees Klap <ceesklap@gmail.com>
2020-11-25 17:28:21 +0100

    moved the app to client folder and added express server in root folder

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishablekey = 'pk_test_51HmilsA191ZPfEqz4e7hRicfXqpWeQSghr9gmU9h3t2GuyGIuaGP3k2dnzdvNqwjaaD5AYnTwNZigz65Zmhs8JpX00cJIGxPc9'
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
+        axios ({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token: token
+            }
+        }).then(responce => {
+            alert('Payment successful')
+        }).catch(error => {
+            console.log('Payment error: ', error )
+            alert
+            ('There was an issue with your payment. Please make sure you use the provided credit Cart.')
+        })
+        
     }
 
     return(
